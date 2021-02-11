c58835503195ed8128269dc5d1f499a4aa4697bb
hairenaa <69110014+hairenaa@users.noreply.github.com>
2021-02-11 21:42:39 +0800

    Add files via upload

---
+++ cs_file/PictureCompression.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureHandler_dll
+{
+    public class PictureCompression : PictureCompressionBase
+    {
+        public override void CompressionORDeCompressionHandleMethod()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ cs_file/PictureCompressionBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PictureHandler_dll
+{
+    public abstract class PictureCompressionBase : PictureBase
+    {
+        public abstract void CompressionORDeCompressionHandleMethod();
+
+
+
+    }
+}
